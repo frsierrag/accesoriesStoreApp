3997bfd6d0c111c8adbf2e7001452e72d7fd4fcc
Fabian Sierra <frsierrag@gmail.com>
2020-12-15 10:14:56 -0500

    1. Update forms to receive fileField and validators to it 2. Update to save an image and get its route 3. Update products_admin.html and update_admin.html to can save images 4. Put method in form in update_user.html

--- blog/app/rutas.py
+++ blog/app/rutas.py
@@ -1,17 +1,20 @@
-import json
+import json, os
 from app import app, bdd
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_login import login_manager, login_user, logout_user, login_required, current_user
+from flask import render_template, request, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
 from app.formularios import FormLogin, FormRecoverPass, FormChangePass, FormRegister, FormCreate, FormUpdate, FormDelete, FormSearch, FormUpdateInventary
 from app.mocks import userValidate, listAccesories
 from app.modelos import Usuario, Producto
 from app.enviar_email import contraseña_olvidada
 from werkzeug.urls import url_parse
+from werkzeug.utils import secure_filename
 
 
-UPLOAD_FOLDER = '/StoreApp/accesoriesStoreApp/blog/app/static/img'
+UPLOAD_FOLDER = 'static\img'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 
 @app.route('/')
@@ -24,8 +27,8 @@ def index():
 def login():
     #     user = userValidate(request.form["userName"], request.form["password"])
     if current_user.is_authenticated:
-        TipoUsuario = current_user.admin
-        if TipoUsuario:
+        tipoUsuario = current_user.admin
+        if tipoUsuario:
             return redirect(url_for('home_admin'))
         else:
             return redirect(url_for('home_user'))
@@ -58,8 +61,8 @@ def login():
 @app.route('/recover_password', methods=['GET', 'POST'])
 def recover_password():
     if current_user.is_authenticated:
-        TipoUsuario = current_user.admin
-        if TipoUsuario:
+        tipoUsuario = current_user.admin
+        if tipoUsuario:
             return redirect(url_for('home_admin'))
         else:
             return redirect(url_for('home_user'))
@@ -81,8 +84,8 @@ def recover_password():
 @app.route('/change_pass/<token>', methods= ['GET', 'POST'])
 def change_pass(token):
     if current_user.is_authenticated:
-        TipoUsuario = current_user.admin
-        if TipoUsuario:
+        tipoUsuario = current_user.admin
+        if tipoUsuario:
             return redirect(url_for('home_admin'))
         else:
             return redirect(url_for('home_user'))
@@ -102,8 +105,8 @@ def change_pass(token):
 @login_required
 def home_admin():
     if current_user.is_authenticated:
-        TipoUsuario = current_user.admin
-        if not TipoUsuario:
+        tipoUsuario = current_user.admin
+        if not tipoUsuario:
             return redirect(url_for('home_user'))        
     return render_template('/home_admin.html')
 
@@ -112,8 +115,8 @@ def home_admin():
 @login_required
 def admin_register():
     if current_user.is_authenticated:
-        TipoUsuario = current_user.admin
-        if not TipoUsuario:
+        tipoUsuario = current_user.admin
+        if not tipoUsuario:
             return redirect(url_for('home_user'))
     form = FormRegister()
     if form.validate_on_submit():
@@ -130,8 +133,8 @@ def admin_register():
 @login_required
 def products_admin():
     if current_user.is_authenticated:
-        TipoUsuario = current_user.admin
-        if not TipoUsuario:
+        tipoUsuario = current_user.admin
+        if not tipoUsuario:
             return redirect(url_for('home_user'))
     formCreate = FormCreate()
     formSearch = FormSearch()
@@ -147,13 +150,11 @@ def products_admin():
                     stateCreate='', formCreate=formCreate)
             else:
                 return render_template('/products_admin.html')
-        elif "create" in request.form:
-            file = request.files['file']
-            # if file and allowed_file(file.filename):
-            #     filename = secure_filename(file.filename)
-            #     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            #     return redirect(url_for('uploaded_file', filename=filename))
-            return render_template('/products_admin.html', formCreate=formCreate)
+        if "image" in request.files:
+            pic = request.files['image']
+            if pic and allowed_file(pic.filename):
+                pic.save(os.path.join(basedir, app.config['UPLOAD_FOLDER'], pic.filename))
+                return render_template('/home_admin.html')
         return render_template('/products_admin.html', formCreate=formCreate, formSearch=formSearch, 
             stateSearch='', stateCreate='is-active',)
 
@@ -162,8 +163,8 @@ def products_admin():
 @login_required
 def update_admin():
     if current_user.is_authenticated:
-        TipoUsuario = current_user.admin
-        if not TipoUsuario:
+        tipoUsuario = current_user.admin
+        if not tipoUsuario:
             return redirect(url_for('home_user'))
     formUpdate = FormUpdate()
     formDelete = FormDelete()
@@ -173,9 +174,12 @@ def update_admin():
         if "accesory" in request.args:
             return render_template('/update_admin.html', formUpdate=formUpdate, searchProduct=product)
     elif request.method == "POST":
-        if formUpdate.validate_on_submit():
-            return render_template('/home_user.html')
-        return render_template('/update_admin.html', formUpdate=formUpdate)
+        if "image" in request.files:
+            pic = request.files['image']
+            if pic and allowed_file(pic.filename):
+                pic.save(os.path.join(basedir, app.config['UPLOAD_FOLDER'], pic.filename))
+                return render_template('/home_admin.html')
+        return render_template('/update_admin.html', formCreate=formUpdate)
     elif request.method == "DELETE":
         return render_template('/update_admin.html', formDelete=formDelete)
 
@@ -184,8 +188,8 @@ def update_admin():
 @login_required
 def home_user():
     if current_user.is_authenticated:
-        TipoUsuario = current_user.admin
-        if TipoUsuario:
+        tipoUsuario = current_user.admin
+        if tipoUsuario:
             return redirect(url_for('home_admin'))        
     return render_template('/home_user.html')
 
@@ -194,8 +198,8 @@ def home_user():
 @login_required
 def products_user():
     if current_user.is_authenticated:
-        TipoUsuario = current_user.admin
-        if TipoUsuario:
+        tipoUsuario = current_user.admin
+        if tipoUsuario:
             return redirect(url_for('home_admin'))
     if request.method == "GET":
         return render_template('/products_user.html')
@@ -212,8 +216,8 @@ def products_user():
 @login_required
 def update_user():
     if current_user.is_authenticated:
-        TipoUsuario = current_user.admin
-        if TipoUsuario:
+        tipoUsuario = current_user.admin
+        if tipoUsuario:
             return redirect(url_for('home_admin'))
     formUpdateInventary = FormUpdateInventary()
     if request.method == "GET":
